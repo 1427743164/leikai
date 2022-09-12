73938699095a13c9b22e93dbf8c39d23764a4ed2
1427743164 <112543212+1427743164@users.noreply.github.com>
2022-09-12 13:39:04 +0800

    Add files via upload

---
+++ leetcode58.java
@@ -0,0 +1,17 @@
+public class leetcode58{
+    public int lengthOfLastWord(String s) {
+        int len=0;
+        for(int i=s.length()-1;i>=0;i--) {
+            if (s.charAt(i)!=' '){
+                len++;
+            } else {
+                if  (len!=0){
+                    break;
+                }
+            }
+
+        }
+        return len;
+
+    }
+}
